33dc18f75f157e63f199624ab500222362dcad69
Jonas-Petrikas <jpetrik15@students.lcc.lt>
2025-02-07 16:35:22 +0200

    list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import './buttons.css';
+import './list.css';
+import List from './Components/List';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <button className='button-53'>Spausk mygtuką, nieko nebus</button>
+        <List />
 
       </header>
     </div>
---
+++ src/Components/List.jsx
@@ -0,0 +1,22 @@
+export default function List({ }) {
+    const tekstai = JSON.stringify(['random tekstas kuris yra mano note', 'kitas random tekstas', 'trečias random tekstas'])
+
+    localStorage.setItem("notes", tekstai);
+
+
+    const notes = JSON.parse(localStorage.getItem("notes"));
+
+    console.log(notes);
+
+
+    return (
+        <>
+            <div className="list-bin">
+                {notes.map((el, i) => <div className="list-item" key={i}> <button className="buttonX">x</button> <span>{el}</span></div>)}
+            </div>
+        </>
+
+
+    );
+
+}
