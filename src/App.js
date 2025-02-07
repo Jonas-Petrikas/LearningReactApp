054ff919d7efac6cd48bcefa9580069d1500f610
Jonas-Petrikas <jpetrik15@students.lcc.lt>
2025-02-07 15:20:10 +0200

    buttons style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import './buttons.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Naaah
-        </a>
+        <button className='button-53'>Spausk mygtuką, nieko nebus</button>
+
       </header>
     </div>
   );
 }
+
 
 export default App;
