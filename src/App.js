30745a488811347fdc1ebca8281ae4b0df694feb
Rokas <opt.imistas@inbox.lt>
2025-02-07 16:42:04 +0200

    notes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import './buttons.css';
+import Input from './Components/Input';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <button className='button-53'>Spausk mygtuką, nieko nebus</button>
-
+        <Input />
       </header>
     </div>
   );
